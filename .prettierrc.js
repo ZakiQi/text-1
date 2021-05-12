3d5f1364db965352a1f2dd913cfac7bb7599c757
huangiyongqi <860029955@qq.com>
2021-05-12 10:51:12 +0800

    -

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  trailingComma: 'all',
+  // trailingComma: 'all',
   // 缩进
   tabWidth: 2,
   // 末尾分号
@@ -10,10 +10,10 @@ module.exports = {
   // 尽可能使用尾随逗号（包括函数参数)
   trailingComma: 'none',
   extends: [
-    //继承 vue 的标准特性
-    "plugin:vue/essential",
-    "eslint:recommended",
-    //避免与 prettier 冲突
-    "plugin:prettier/recommended",
+    // 继承 vue 的标准特性
+    'plugin:vue/essential',
+    'eslint:recommended',
+    // 避免与 prettier 冲突
+    'plugin:prettier/recommended'
   ]
 }
